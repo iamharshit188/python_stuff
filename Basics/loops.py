334752a60652386b0358831514ae3f1151fb4900
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2026-02-04 11:35:46 +0530

    Added iterations and loops

---
+++ Basics/loops.py
@@ -0,0 +1,18 @@
+# Iteration in python
+# for loop
+
+print("----For loop ----")
+for i in range(5):
+    print(i)
+ 
+print("----While loop ----")
+count = 0 
+while count < 5 :
+    print(count)
+    count +=1
+
+print("---- Looping over string -----")
+text = "God is a Woman"
+for words in text:
+    print(words)
+    
