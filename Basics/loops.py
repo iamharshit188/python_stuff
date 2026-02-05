279a64ab041596564743aa7441a2c3f163c3bede
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2026-02-05 12:35:35 +0530

    added an info :_

--- Basics/loops.py
+++ Basics/loops.py
@@ -4,6 +4,9 @@
 print("----For loop ----")
 for i in range(5):
     print(i)
+# we can also pass _ instead of i if we don't want to use the variable
+for _ in range(5):
+    print("Hello, World!")
  
 print("----While loop ----")
 count = 0 
