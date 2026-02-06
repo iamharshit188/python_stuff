e8a5e8cbee22999e6ae47b9e662db00c9d4768e7
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2026-02-06 11:41:09 +0530

    added few operations and examples

--- Basics/BasicDataStructures.py
+++ Basics/BasicDataStructures.py
@@ -3,3 +3,13 @@ print("----- Topic: Basic Data Structures -----")
 # List
 print("Subtopic: List")
 my_list = [1, 2, 3, 4, 5]
+fruits = ["apple", "banana", "cherry"]
+mixed = [1, "hello", True, 3.5]
+
+print("Value of my_list:", my_list)
+print("Value of mixed" , mixed)
+print("Type of my_list:", type(my_list))
+# Lists are ordered, mutable collections of items. They can contain elements of different data types and are defined using square brackets [].
+print(fruits[0]) # O ==> 1st element that is apple
+print(fruits[2]) # 2 ==> 3rd element that is cherry
+print(fruits[-1]) # -1 ==> last element that is cherry
